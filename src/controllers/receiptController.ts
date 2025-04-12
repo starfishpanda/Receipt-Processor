149ad5b29f8fa1ea3f732745864adf837095c1c7
Philip Brown <philip.murga@gmail.com>
2025-04-12 11:20:42 -0700

    Initial commit.

---
+++ src/controllers/receiptController.ts
@@ -0,0 +1,10 @@
+import { Request, Response, NextFunction } from 'express'
+import axios from 'axios'
+
+const receiptController = {
+  // processReceipts
+
+  // getPoints
+}
+
+export default receiptController
